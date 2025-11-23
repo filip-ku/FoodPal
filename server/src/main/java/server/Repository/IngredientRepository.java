cdb1489b0f6dc3cfac1a701944f0949a60f55c71
Ivan Gvardeitsev <I.S.Gvardeitsev@student.tudelft.nl>
2025-11-23 22:14:55 +0100

    Set up ingredient and recipe repositories

---
+++ server/src/main/java/server/Repository/IngredientRepository.java
@@ -0,0 +1,10 @@
+package server.Repository;
+
+import commons.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * TODO add actual ingredient repo information
+ */
+public interface IngredientRepository {
+}
