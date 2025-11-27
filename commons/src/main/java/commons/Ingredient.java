cc94076e58b61c0c2c94331c5d20ea3b0aad7994
Max ten Broeke <m.broeke@student.tudelft.nl>
2025-11-27 23:33:21 +0100

    minor changes to ingredient entity to fix errors

--- commons/src/main/java/commons/Ingredient.java
+++ commons/src/main/java/commons/Ingredient.java
@@ -1,10 +1,8 @@
 package commons;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Ingredient {
 
     @Id
