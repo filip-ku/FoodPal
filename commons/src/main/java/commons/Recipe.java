7eb37806a180ec0a1645aa00b16fa1cb18ebd1ce
Merlijn Koster <M.T.T.Koster@student.tudelft.nl>
2025-11-29 00:03:45 +0100

    Merge branch 'main' into 'Backend-tests'
    # Conflicts:
    #   commons/src/main/java/commons/Recipe.java

--- commons/src/main/java/commons/Recipe.java
+++ commons/src/main/java/commons/Recipe.java
@@ -2,6 +2,9 @@ package commons;
 
 import jakarta.persistence.*;
 
+/**
+ * JPA entity representing a recipe.
+ */
 @Entity
 public class Recipe {
     @Id
@@ -16,20 +19,47 @@ public class Recipe {
      */
     public Recipe() {}
 
+    /**
+     * Creates a recipe with the given title.
+     *
+     * @param title non‑null title.
+     * @throws IllegalArgumentException if {@code title} is null.
+     */
     public Recipe(String title) {
-        this.title = title;
+        setTitle(title);
     }
 
+    /**
+     * Returns the generated id.
+     * @return the generated id.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets a new id for this recipe.
+     * This method should only be used for testing.
+     * Normally the recipe id will be set by the server.
+     *
+     * @param id new id that will be set by this method
+     */
     public void setId(Long id) {this.id = id;}
 
+    /**
+     * Returns the recipe’s title.
+     * @return the recipe’s title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets a new title for this recipe.
+     *
+     * @param title non‑null title.
+     * @throws IllegalArgumentException if {@code title} is null.
+     */
     public void setTitle(String title) {
         if (title == null) {
             throw new IllegalArgumentException("title cannot be null");
